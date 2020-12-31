feeb7ba519959d3229fa89012c2d811ef692bdc7
iordachebogdan <iordache_bogdan1998@yahoo.com>
2020-12-31 10:43:11 +0200

    Add emotes

--- lib/preprocessing/cleaning.py
+++ lib/preprocessing/cleaning.py
@@ -1,5 +1,6 @@
 import re
 from emoji import demojize
+import emot
 
 
 def basic_clean(text):
@@ -13,11 +14,22 @@ def basic_clean(text):
 
 def clean(text):
     text = text.lower()
-    text = demojize(text, delimiters=(" #", "# "))
     text = re.sub(r"@[^\s]+", " #user_handle# ", text)
     text = re.sub(r"https?://[^ ]+", " #url# ", text)
     text = re.sub(r"www.[^ ]+", " #url# ", text)
-    text = re.sub(r"[^a-zA-Zäöüß_#]", " ", text)
+    text = demojize(text, delimiters=(" #", "# "))
+    while True:
+        emoticons = emot.emoticons(text)
+        if not isinstance(emoticons, dict) or not emoticons["flag"]:
+            break
+        text = (
+            text[: emoticons["location"][0][0]]
+            + " #"
+            + "_".join(emoticons["mean"][0].lower().split(" "))
+            + "# "
+            + text[emoticons["location"][0][1] :]
+        )
+    text = re.sub(r"[^a-zA-Zäöüß_#-]", " ", text)
     text = re.sub(r"\s+", " ", text)
     text = text.strip()
     return text
