c6949ace7a2f23387fce3a1a8fee63d4fb860bd9
iordachebogdan <iordache_bogdan1998@yahoo.com>
2021-01-05 15:01:12 +0200

    LinearSVR stats

--- lib/preprocessing/cleaning.py
+++ lib/preprocessing/cleaning.py
@@ -3,19 +3,27 @@ import emot
 
 
 def basic_clean(text):
+    # remove usernames
     text = re.sub(r"@[a-zA-Z0-9äöüÄÖÜß]+", " ", text)
+    # remove urls
     text = re.sub(r"https?://[^ ]+", " ", text)
     text = re.sub(r"www.[^ ]+", " ", text)
+    # remove non-letter symbols
     text = re.sub(r"[^a-zA-ZäöüÄÖÜß]", " ", text)
+    # contract spaces
     text = re.sub(r" +", " ", text)
     return text
 
 
 def clean(text):
+    # lowercase
     text = text.lower()
+    # replace usernames
     text = re.sub(r"@[^\s]+", " #user_handle# ", text)
+    # replace urls
     text = re.sub(r"https?://[^ ]+", " #url# ", text)
     text = re.sub(r"www.[^ ]+", " #url# ", text)
+    # replace emoji
     while True:
         emoji = emot.emoji(text)
         if not isinstance(emoji, dict) or not emoji["flag"]:
@@ -32,6 +40,7 @@ def clean(text):
             + "# "
             + text[emoji["location"][0][1] + 1 :]
         )
+    # replace emoticons
     while True:
         emoticons = emot.emoticons(text)
         if not isinstance(emoticons, dict) or not emoticons["flag"]:
@@ -48,7 +57,9 @@ def clean(text):
             + "# "
             + text[emoticons["location"][0][1] :]
         )
+    # remove other symbols
     text = re.sub(r"[^a-zA-Zäöüß_#]", " ", text)
+    # contract spaces
     text = re.sub(r"\s+", " ", text)
     text = text.strip()
     return text
